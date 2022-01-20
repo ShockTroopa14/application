2e7d8fbcb166cd0f49426a9218ed9dc5c0ea9a42
ShockTroopa14 <52359305+ShockTroopa14@users.noreply.github.com>
2022-01-19 20:13:36 -0500

    first commit

---
+++ shopify.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 18 18:54:27 2022
+
+@author: Alex Green
+"""
+import csv
+
+
+file = open('datafile.csv')
+
+type(file)
+
+csvreader = csv.reader(file)
+
+
+
+
